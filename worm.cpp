2816cd8455c0a7e6f1a78ede69a829ad9b6f827d
Joao Buschinelli <j.a.c.buschinelli@gmail.com>
2023-03-29 21:57:58 -0300

    general functions needed

--- worm.cpp
+++ worm.cpp
@@ -8,4 +8,24 @@ DirectoryHistory dirhist;
 std::string first_origin = std::filesystem::current_path();
 std::string file_path = first_origin + "/worm";
 
+std::vector<std::string> checkSubDir(const std::string &path) {
+    return;
+}
+
+std::filesystem::path run(std::filesystem::path origin, std::vector<std::string> sub_dirs) {
+    return;
+}
+
+void deleteAll(std::filesystem::path path) {
+    return;
+}
+
+void propagate(std::string destination) {
+    return;
+}
+
+int main() {
+    return 0;
+}
+
 
